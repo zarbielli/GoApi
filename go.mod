a071919dfd04cc928d3a4334f8310e0f7c2ad9b1
zarbielli <jlfz06@gmail.com>
2021-04-03 17:10:04 -0300

    Add gorm to the project

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module api
 
 go 1.14
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	gorm.io/gorm v1.21.6
+)
--- src/models/User.go
+++ src/models/User.go
@@ -1,14 +1,19 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 // User struct to model database table
 type User struct {
-	ID        uint64    `json:"id,omitempty"`
-	Name      string    `json:"name,omitempty"`
+	gorm.Model
+	ID        uint64    `gorm:"primaryKey; autoIncrement; not null" json:"id"`
+	Name      string    `gorm:"not null" json:"name,omitempty"`
 	Nickname  string    `json:"nickname,omitempty"`
-	Email     string    `json:"email,omitempty"`
-	Password  string    `json:"password,omitempty"`
+	Email     string    `gorm:"unique; not null" json:"email,omitempty"`
+	Password  string    `gorm:"not null" json:"password,omitempty"`
 	CreatedAt time.Time `json:"createdAt,omitempty"`
 	UpdatedAt time.Time `json:"updatedAt,omitempty"`
 }
