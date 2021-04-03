acc6a14b002c412c806a0ef1e105f5e9d626a569
zarbielli <jlfz06@gmail.com>
2021-04-03 16:19:56 -0300

    Creating user model

---
+++ src/Models/User.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+// User struct to model database table
+type User struct {
+	ID        uint64    `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Nickname  string    `json:"nickname,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Password  string    `json:"password,omitempty"`
+	CreatedAt time.Time `json:"createdAt,omitempty"`
+	UpdatedAt time.Time `json:"updatedAt,omitempty"`
+}
